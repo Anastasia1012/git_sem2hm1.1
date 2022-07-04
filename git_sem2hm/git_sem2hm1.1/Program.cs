d8c52b40e7ffd8de925ea3bb645f4765f950f36c
Anastasia1012 <107638644+Anastasia1012@users.noreply.github.com>
2022-07-04 19:25:53 +0300

    Add files via upload

---
+++ git_sem2hm/git_sem2hm1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("input the programm which inputs three-digit numbers and outputs the second");
+int numb = Convert.ToInt32(Console.ReadLine());
+numb =(numb / 10)%10;
+Console.WriteLine(numb);
+
---
+++ git_sem2hm/git_sem2hm1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+/*Console.WriteLine("input the programm which inputs three-digit numbers and outputs the third or information that it doesn't exist");
+int numb = Convert.ToInt32(Console.ReadLine());
+numb =(numb / 10)%10;
+for(numb;numb < 1000;numb / 10);
+{
+}
+Console.WriteLine(numb);*/
+
